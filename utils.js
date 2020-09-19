b8926e7d4d1660385b3ebb554b82aabf78770f2a
Rodrigo <rsedano@deloittedigital.es>
2020-09-19 17:16:35 +0200

    Button Helpers

--- utils.js
+++ utils.js
@@ -1,5 +1,5 @@
 // gameOS theme
-// Copyright (C) 2018-2020 Seth Powell 
+// Copyright (C) 2018-2020 Seth Powell
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -16,51 +16,43 @@
 
 // This file contains some helper scripts for formatting data
 
-
 // For multiplayer games, show the player count as '1-N'
 function formatPlayers(playerCount) {
-    if (playerCount === 1)
-        return playerCount
+  if (playerCount === 1) return playerCount;
 
-    return "1-" + playerCount;
+  return "1-" + playerCount;
 }
-
 
 // Show dates in Y-M-D format
 function formatDate(date) {
-    return Qt.formatDate(date, "yyyy-MM-dd");
+  return Qt.formatDate(date, "yyyy-MM-dd");
 }
-
 
 // Show last played time as text. Based on the code of the default Pegasus theme.
 // Note to self: I should probably move this into the API.
 function formatLastPlayed(lastPlayed) {
-    if (isNaN(lastPlayed))
-        return "never";
+  if (isNaN(lastPlayed)) return "never";
 
-    var now = new Date();
+  var now = new Date();
 
-    var elapsedHours = (now.getTime() - lastPlayed.getTime()) / 1000 / 60 / 60;
-    if (elapsedHours < 24 && now.getDate() === lastPlayed.getDate())
-        return "today";
+  var elapsedHours = (now.getTime() - lastPlayed.getTime()) / 1000 / 60 / 60;
+  if (elapsedHours < 24 && now.getDate() === lastPlayed.getDate())
+    return "today";
 
-    var elapsedDays = Math.round(elapsedHours / 24);
-    if (elapsedDays <= 1)
-        return "yesterday";
+  var elapsedDays = Math.round(elapsedHours / 24);
+  if (elapsedDays <= 1) return "yesterday";
 
-    return elapsedDays + " days ago"
+  return elapsedDays + " days ago";
 }
-
 
 // Display the play time (provided in seconds) with text.
 // Based on the code of the default Pegasus theme.
 // Note to self: I should probably move this into the API.
 function formatPlayTime(playTime) {
-    var minutes = Math.ceil(playTime / 60)
-    if (minutes <= 90)
-        return Math.round(minutes) + " minutes";
+  var minutes = Math.ceil(playTime / 60);
+  if (minutes <= 90) return Math.round(minutes) + " minutes";
 
-    return parseFloat((minutes / 60).toFixed(1)) + " hours"
+  return parseFloat((minutes / 60).toFixed(1)) + " hours";
 }
 
 // Process the platform name to make it friendly for the logo
@@ -241,16 +233,16 @@ function processButtonArt(button) {
   var buttonModel;
   switch (button) {
     case "accept":
-      buttonModel = api.keys.accept;
+      buttonModel = screen.width == 640 ? api.keys.cancel : api.keys.accept;
       break;
     case "cancel":
-      buttonModel = api.keys.cancel;
+      buttonModel = screen.width == 640 ? api.keys.accept : api.keys.cancel;
       break;
     case "filters":
-      buttonModel = api.keys.filters;
+      buttonModel = screen.width == 640 ? api.keys.details : api.keys.filters;
       break;
     case "details":
-      buttonModel = api.keys.details;
+      buttonModel = screen.width == 640 ? api.keys.filters : api.keys.details;
       break;
     case "nextPage":
       buttonModel = api.keys.nextPage;
@@ -261,9 +253,9 @@ function processButtonArt(button) {
     case "pageUp":
       buttonModel = api.keys.pageUp;
       break;
-      case "pageDown":
-        buttonModel = api.keys.pageDown;
-        break;
+    case "pageDown":
+      buttonModel = api.keys.pageDown;
+      break;
     default:
       buttonModel = api.keys.accept;
   }
@@ -271,51 +263,44 @@ function processButtonArt(button) {
   var i;
   for (i = 0; buttonModel.length; i++) {
     if (buttonModel[i].name().includes("Gamepad")) {
-      var buttonValue = buttonModel[i].key.toString(16)
-      return buttonValue.substring(buttonValue.length-1, buttonValue.length);
+      var buttonValue = buttonModel[i].key.toString(16);
+      return buttonValue.substring(buttonValue.length - 1, buttonValue.length);
     }
   }
 }
 
-function steamAppID (gameData) {
+function steamAppID(gameData) {
   var str = gameData.assets.boxFront.split("header");
   return str[0];
 }
 
 function steamBoxArt(gameData) {
-  return steamAppID(gameData) + '/library_600x900_2x.jpg';
+  return steamAppID(gameData) + "/library_600x900_2x.jpg";
 }
 
 function steamLogo(gameData) {
-  return steamAppID(gameData) + "/logo.png"
+  return steamAppID(gameData) + "/logo.png";
 }
 
 function steamHero(gameData) {
-  return steamAppID(gameData) + "/library_hero.jpg"
+  return steamAppID(gameData) + "/library_hero.jpg";
 }
 
 // Just use boxFront?
 function steamHeader(gameData) {
-  return steamAppID(gameData) + "/header.jpg"
+  return steamAppID(gameData) + "/header.jpg";
 }
 
 function boxArt(data) {
   if (data != null) {
-    if (data.assets.boxFront.includes("/header.jpg")) 
-      return steamBoxArt(data);
+    if (data.assets.boxFront.includes("/header.jpg")) return steamBoxArt(data);
     else {
-      if (data.assets.boxFront != "")
-        return data.assets.boxFront;
-      else if (data.assets.poster != "")
-        return data.assets.poster;
-      else if (data.assets.banner != "")
-        return data.assets.banner;
-      else if (data.assets.tile != "")
-        return data.assets.tile;
-      else if (data.assets.cartridge != "")
-        return data.assets.cartridge;
-      else if (data.assets.logo != "")
-        return data.assets.logo;
+      if (data.assets.boxFront != "") return data.assets.boxFront;
+      else if (data.assets.poster != "") return data.assets.poster;
+      else if (data.assets.banner != "") return data.assets.banner;
+      else if (data.assets.tile != "") return data.assets.tile;
+      else if (data.assets.cartridge != "") return data.assets.cartridge;
+      else if (data.assets.logo != "") return data.assets.logo;
     }
   }
   return "";
@@ -323,11 +308,9 @@ function boxArt(data) {
 
 function logo(data) {
   if (data != null) {
-    if (data.assets.boxFront.includes("/header.jpg")) 
-      return steamLogo(data);
+    if (data.assets.boxFront.includes("/header.jpg")) return steamLogo(data);
     else {
-      if (data.assets.logo != "")
-        return data.assets.logo;
+      if (data.assets.logo != "") return data.assets.logo;
     }
   }
   return "";
@@ -335,13 +318,10 @@ function logo(data) {
 
 function fanArt(data) {
   if (data != null) {
-    if (data.assets.boxFront.includes("/header.jpg")) 
-      return steamHero(data);
+    if (data.assets.boxFront.includes("/header.jpg")) return steamHero(data);
     else {
-      if (data.assets.background != "")
-        return data.assets.background;
-      else if (data.assets.screenshots[0])
-        return data.assets.screenshots[0];
+      if (data.assets.background != "") return data.assets.background;
+      else if (data.assets.screenshots[0]) return data.assets.screenshots[0];
     }
   }
   return "";
@@ -349,9 +329,9 @@ function fanArt(data) {
 
 // Place Steam collections at the beginning of the list
 function reorderCollection(model) {
-  for(var i=0; i<model.count; i++) {
+  for (var i = 0; i < model.count; i++) {
     if (model.get(i).name == "Steam") {
-      model.move(i,0);
+      model.move(i, 0);
       return model;
     }
   }
@@ -359,29 +339,31 @@ function reorderCollection(model) {
 }
 
 // Shuffle function
-function shuffle(model){
-  var currentIndex = model.count, temporaryValue, randomIndex;
+function shuffle(model) {
+  var currentIndex = model.count,
+    temporaryValue,
+    randomIndex;
 
   // While there remain elements to shuffle...
   while (0 !== currentIndex) {
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex)
-      currentIndex -= 1
-      // And swap it with the current element.
-      // the dictionaries maintain their reference so a copy should be made
-      // https://stackoverflow.com/a/36645492/6622587
-      temporaryValue = JSON.parse(JSON.stringify(model.get(currentIndex)))
-      model.set(currentIndex, model.get(randomIndex))
-      model.set(randomIndex, temporaryValue);
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    // And swap it with the current element.
+    // the dictionaries maintain their reference so a copy should be made
+    // https://stackoverflow.com/a/36645492/6622587
+    temporaryValue = JSON.parse(JSON.stringify(model.get(currentIndex)));
+    model.set(currentIndex, model.get(randomIndex));
+    model.set(randomIndex, temporaryValue);
   }
-  
+
   return model;
 }
 
 function uniqueGameValues(fieldName) {
   const set = new Set();
-  api.allGames.toVarArray().forEach(game => {
-      game[fieldName].forEach(v => set.add(v));
+  api.allGames.toVarArray().forEach((game) => {
+    game[fieldName].forEach((v) => set.add(v));
   });
   return [...set.values()].sort();
 }
@@ -389,26 +371,27 @@ function uniqueGameValues(fieldName) {
 function uniqueValuesArray(fieldName) {
   let arr = [];
   var allGames = api.allGames.toVarArray();
-  for(var i=0;i<allGames.length;i++) {
+  for (var i = 0; i < allGames.length; i++) {
     arr.push(allGames[i][fieldName]);
   }
   return arr;
 }
 
 function shuffleArray(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
 
   // While there remain elements to shuffle...
   while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
 
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-
-      // And swap it with the current element.
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
   }
 
   return array;
